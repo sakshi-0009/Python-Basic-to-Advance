e9fd43debe35cef2a5e21989a1768661264bde05
sakshi-0009 <sakshigome1809@gmail.com>
2023-10-06 06:58:38 +0530

    Numeric data type

---
+++ Practice_Codes/Data Types/01_Numeric.py
@@ -0,0 +1,19 @@
+''' Numeric data type have three types : 1.int, 2.float, 3.complexData'''
+
+#   1.int
+
+empId = 101
+print(empId)
+print(type(empId))  #<class 'int'>
+
+#   2.float
+
+empSalary = 450000.50
+print(empSalary)
+print(type(empSalary))  #<class 'float'>
+
+#   3.complex
+
+complexData = 10+7j
+print(complexData)
+print(type(complexData))    #<class 'complex'>
