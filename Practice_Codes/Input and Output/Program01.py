b5ec69eab0e226e2756b94cf50d8629e74657012
sakshi-0009 <sakshigome1809@gmail.com>
2023-10-09 00:04:13 +0530

    Taking input from user

---
+++ Practice_Codes/Input and Output/Program01.py
@@ -0,0 +1,9 @@
+# Take input from user of name and marks of two students.abs
+
+name1 = input("Enter student name : ")
+marks1 = int(input("Enter marks : "))
+print(name1," = ",marks1)
+
+name2 = input("Enter student name : ")
+marks2 = int(input("Enter marks : "))
+print(name2," = ",marks2)
---
+++ Practice_Codes/Input and Output/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+name1 = input("Enter student name : ")
+# marks1 = int(input("Enter marks : "))
+# print(name1," = ",marks1)
+
+# name2 = input("Enter student name : ")
+# marks2 = int(input("Enter marks : "))
+# print(name2," = ",marks2)
