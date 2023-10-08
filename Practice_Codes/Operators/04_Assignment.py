294380c71c48e9e915423aff39b3346c933deb5c
sakshi-0009 <sakshigome1809@gmail.com>
2023-10-08 23:47:21 +0530

    Assignment operator

---
+++ Practice_Codes/Operators/04_Assignment.py
@@ -0,0 +1,8 @@
+x = 10
+y = 5
+
+print(x = x + y)
+print(x = x - y)
+print(x = x * y)
+print(x = x / y)
+
---
+++ Practice_Codes/Operators/05_Identity.py
@@ -0,0 +1,7 @@
+listData = [10,20,30,40,50]
+x = 10
+y = 60
+print(x in listData)
+print(y in listData)
+print(x not in listData)
+print(y not in listData)
