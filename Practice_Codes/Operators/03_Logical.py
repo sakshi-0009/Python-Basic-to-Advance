f47499958499cffc31212cc94888b85402f0fdb2
sakshi-0009 <sakshigome1809@gmail.com>
2023-10-08 23:19:48 +0530

    Logical operator

--- Practice_Codes/Operators/03_Logical.py
+++ Practice_Codes/Operators/03_Logical.py
@@ -5,3 +5,11 @@ print(x and y)  #False
 print(x or y)   #True
 print(not x)    #False
 print(not y)    #True
+
+a = 0
+b = 20
+
+print(a and b)  #0
+print(a or b)   #20
+print(not a)    #True
+print(not b)    #False
