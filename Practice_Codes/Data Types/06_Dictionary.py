96bb29e43bb556bdb3a9ddeea7fce95daad66c32
sakshi-0009 <sakshigome1809@gmail.com>
2023-10-08 23:04:16 +0530

    Dictionary type

---
+++ Practice_Codes/Data Types/06_Dictionary.py
@@ -0,0 +1,6 @@
+player = {18:"Virat",7:"MSD",1:"KLRahul"}
+print(player)           #{18: 'Virat', 7: 'MSD', 1: 'KLRahul'}
+print(type(player))     #<class 'dict'>
+
+player [7] = "MSDhoni"
+print(player)           #{18: 'Virat', 7: 'MSDhoni', 1: 'KLRahul'}
