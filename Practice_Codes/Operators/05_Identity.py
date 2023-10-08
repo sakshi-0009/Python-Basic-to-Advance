6be83208a22f46098f7c41938d0c170b81f64cbc
sakshi-0009 <sakshigome1809@gmail.com>
2023-10-08 23:48:50 +0530

    Identity operator

--- Practice_Codes/Operators/05_Identity.py
+++ Practice_Codes/Operators/05_Identity.py
@@ -1,7 +1,10 @@
 listData = [10,20,30,40,50]
 x = 10
 y = 60
-print(x in listData)
-print(y in listData)
-print(x not in listData)
-print(y not in listData)
+# 1.in
+print(x in listData)    #Trur
+print(y in listData)    #False
+
+# 2.not in
+print(x not in listData)    #False
+print(y not in listData)    #True
