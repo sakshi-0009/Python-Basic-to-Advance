6cf23f5b407b7570594e42b7bd4f3f73b8f7999d
sakshi-0009 <sakshigome1809@gmail.com>
2023-10-08 23:50:00 +0530

    Membership operator

---
+++ Practice_Codes/Operators/06_Membership.py
@@ -0,0 +1,10 @@
+listData = [10,20,30,40,50]
+x = 10
+y = 60
+# 1.in
+print(x in listData)    #Trur
+print(y in listData)    #False
+
+# 2.not in
+print(x not in listData)    #False
+print(y not in listData)    #True
