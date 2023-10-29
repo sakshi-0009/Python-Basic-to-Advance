cd349a8cdf587a96cb2bd2cbe84b3422dad141fb
sakshi-0009 <sakshigome1809@gmail.com>
2023-10-30 01:03:45 +0530

    Program-02

---
+++ Assignment_Codes/Assignment_01_IfElse/Program_02.py
@@ -0,0 +1,7 @@
+a= int (input("Enter the number: "))
+if(a>0):
+    print("Number is positivve")
+elif(a<0):
+    print("Number is negative")
+else:
+    print("Number is equal to zero!")
--- Assignment_Codes/Assignment_01_IfElse/code2.py
+++
@@ -1,7 +0,0 @@
-a= int (input("enter the number: "))
-if(a>0):
-    print("number is positivve")
-elif(a<0):
-    print("number is negative")
-else:
-    print("number is equal to zero")
