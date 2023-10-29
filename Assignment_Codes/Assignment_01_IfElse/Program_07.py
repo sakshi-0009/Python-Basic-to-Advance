2c18d21be2a213099ec2a3834a64043037b0340f
sakshi-0009 <sakshigome1809@gmail.com>
2023-10-30 01:19:41 +0530

    Program-07

---
+++ Assignment_Codes/Assignment_01_IfElse/Program_07.py
@@ -0,0 +1,27 @@
+Code=int(input("Enter the code for Month : "))
+if(Code==1):
+    print("January is a 31-day's month")
+elif(Code==2):
+    print("February is a 28-day's month")
+elif(Code==3):
+    print("March is a 31-day's month")
+elif(Code==4):
+    print("April is a 30-day's month")
+elif(Code==5):
+    print("May is a 31-day's month")
+elif(Code==6):
+    print("June is a 30-day's month")
+elif(Code==7):
+    print("July is a 31-day's month")
+elif(Code==8):
+    print("August is a 31-day's month")
+elif(Codea==9):
+    print("September is a 30-day's month")
+elif(Code==10):
+    print("Octomber is a 31-day's month")
+elif(Code==11):
+    print("November is a 30-day's month")
+elif(Code==12):
+    print("December is a 31-day's month")
+else:
+    print("There is no month")
