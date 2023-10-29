621a19bb643f00be9dff025984098cdcbb572df4
sakshi-0009 <sakshigome1809@gmail.com>
2023-10-30 01:12:41 +0530

    Program-05

---
+++ Assignment_Codes/Assignment_01_IfElse/Program_05.py
@@ -0,0 +1,17 @@
+Day=int(input("Enter Day code : "))
+if(Day==0):
+    print("Today is Monday")
+elif(Day==1):
+    print("Today is Tuesday")
+elif(Day==2):
+    print("Today is Wednesday")
+elif(Day==3):
+    print("Today is Thursday")
+elif(Day==4):
+    print("Today is Friday")
+elif(Day==5):
+    print("Today is Saturday") 
+elif(Day==6):
+    print("Today is Sunday")
+else:
+    print("Enter Day code between 0 to 6 only!!")
