0bbdcbe122bfd08a0db19f41b9e10ba775d8f227
sakshi-0009 <sakshigome1809@gmail.com>
2023-10-30 01:21:49 +0530

    Program-08

---
+++ Assignment_Codes/Assignment_01_IfElse/Program_08.py
@@ -0,0 +1,5 @@
+num = int (input("Enter the number: "))
+if(num > 10):
+    print("YES!",num, "is greater than 10.")
+else:
+    print("NO!",num, "is less than 10.")
--- Assignment_Codes/Assignment_01_IfElse/code8.py
+++
@@ -1,5 +0,0 @@
-a= int (input("enter the number: "))
-if(a>10):
-    print("yes",a, "is greater than 10")
-else:
-    print("no",a, "is less than 10")
