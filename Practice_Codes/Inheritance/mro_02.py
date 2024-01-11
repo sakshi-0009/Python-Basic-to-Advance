f29eb4723774ae9046c4da6f6d57cfe9b91f91cb
sakshi-0009 <sakshigome1809@gmail.com>
2024-01-11 22:17:33 +0530

    Multiple Inheritance : mro_02

---
+++ Practice_Codes/Inheritance/mro_02.py
@@ -0,0 +1,19 @@
+#Diamond Problem : 
+
+class GrandP:
+    def Fun(self):
+        print("Grand Parents")
+
+class Parent1(GrandP):
+    def Fun(self):
+        print("Child of Parent 1")
+
+class Parent2(GrandP):
+    def Fun(self):
+        print("Child of Parent 2")
+
+class Child(Parent2, Parent1):
+    pass
+
+obj = Child()
+obj.Fun()
