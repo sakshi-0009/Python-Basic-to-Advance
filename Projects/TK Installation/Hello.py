5d700f194aa94abc22f95c0a53e709fbf6e67af9
sakshi-0009 <sakshigome1809@gmail.com>
2024-01-25 12:32:40 +0530

    PyQt Installation

---
+++ Projects/TK Installation/Hello.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+
+class HelloCore2WebApp:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title('Hello Core2Web')
+        
+        self.root.geometry('300x200')
+        
+        self.label = tk.Label(self.root,text="")
+        self.label.pack(pady=10)
+        
+        self.hello_button = tk.Button(self.root,text='Click me',command=self.display_message)
+        self.hello_button.pack(pady=10)
+        
+    def display_message(self):
+        self.label.config(text='Hello Core2Web')
+        
+    def run(self):
+        self.root.mainloop()
+        
+if __name__ == '__main__':
+    app = HelloCore2WebApp()
+    app.run()
