93e3452faf64f775eed9d953230a5e9ce2950a86
sakshi-0009 <sakshigome1809@gmail.com>
2024-01-27 11:20:22 +0530

    Value Error

--- Practice_Codes/Exception Handling/ZeroDivisionError.py
+++ Practice_Codes/Exception Handling/ZeroDivisionError.py
@@ -1,12 +1,10 @@
-# Value Error :-
-
-def Add(a, b):
-    return a+b
+def add(x, y):
+    return x//y
 
 num1 = int(input("Enter num1 : "))
 num2 = int(input("Enter num2 : "))
 
-ans = Add(num1,num2)
+ans = num1+num2
 print("Addition is: ",ans)
 
 # if num1 = 1 and num2 = sakshi --> ValueError: invalid literal for int() with base 10: 'sakshi'
