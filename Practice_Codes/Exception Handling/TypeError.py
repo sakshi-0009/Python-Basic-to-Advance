18486f4b93a1aac262c733e08614524441287d39
sakshi-0009 <sakshigome1809@gmail.com>
2024-01-27 10:59:04 +0530

    Type Error

---
+++ Practice_Codes/Exception Handling/TypeError.py
@@ -0,0 +1,11 @@
+# Type Error :-
+
+# eg.1 :-
+def fun(a, b):
+    print(a)
+    print(b)
+
+print("Start code")
+fun(10,20)
+# fun(10)     TypeError: fun() takes 0 positional arguments but 2 were given
+print("End code")
