1534e11f9936b902d9ff241948079f9ddfdf24cf
sakshi-0009 <sakshigome1809@gmail.com>
2024-01-27 11:21:35 +0530

    Zero Division Error

--- Practice_Codes/Exception Handling/ZeroDivisionError.py
+++ Practice_Codes/Exception Handling/ZeroDivisionError.py
@@ -1,10 +1,10 @@
-def add(x, y):
+def Div(x, y):
     return x//y
 
 num1 = int(input("Enter num1 : "))
 num2 = int(input("Enter num2 : "))
 
-ans = num1+num2
+ans = Div(num1,num2)
 print("Addition is: ",ans)
 
-# if num1 = 1 and num2 = sakshi --> ValueError: invalid literal for int() with base 10: 'sakshi'
+# if num1 = 145 and num2 = 0 --> ZeroDivisionError: integer division or modulo by zero
